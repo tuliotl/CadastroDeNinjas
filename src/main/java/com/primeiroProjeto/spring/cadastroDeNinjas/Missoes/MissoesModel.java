21ac890378d0c1c58d5ce2d9c6e0899db9dcc7f4
0xLight <tuliosoares12@gmail.com>
2026-01-13 21:46:07 +0000

    feat: Coloca o lombok na classe para abstração

--- src/main/java/com/primeiroProjeto/spring/cadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/com/primeiroProjeto/spring/cadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,14 +2,31 @@ package com.primeiroProjeto.spring.cadastroDeNinjas.Missoes;
 
 import com.primeiroProjeto.spring.cadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+// Entity tranformar uma class em uma entidade no BD
+// Só temos acesso ao entity quando baixamos o SpringBoot JPA
 @Entity
-@Table (name = "tb_missoes")
+
+
+@Table (name = "tb_missoes") // Cria uma tabela e podemos criar tambem o nome da tabela
+
+@NoArgsConstructor // Cria um construtor vazio
+
+@AllArgsConstructor // Cria um construtor com todos os parametos
+
+@Data // Cria todos os getts e setts
+
 public class MissoesModel {
 
+    // ID usado para o java conseguir diferenciar cada usuario no BD
+    // Vai criar na sequencia até o infinito de IDs no BD, começando em 1
     @Id
+    // Gera sequencialmente o id no banco de dados
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
--- src/main/java/com/primeiroProjeto/spring/cadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/primeiroProjeto/spring/cadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,6 +2,9 @@ package com.primeiroProjeto.spring.cadastroDeNinjas.Ninjas;
 
 import com.primeiroProjeto.spring.cadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -9,56 +12,25 @@ import java.util.List;
 // Só temos acesso ao entity quando baixamos o SpringBoot JPA
 @Entity
 
-// Cria uma tabela e podemos criar tambem o nome da tabela
-@Table(name = "tb_cadastro")
+@Table(name = "tb_cadastro") // Cria uma tabela e podemos criar tambem o nome da tabela
 
+@NoArgsConstructor // Cria um construtor vazio
+
+@AllArgsConstructor // Cria um construtor com todos os parametos
+
+@Data // Cria todos os getts e setts
 public class NinjaModel {
 
     @Id // ID usado para o java conseguir diferenciar cada usuario no BD
     // Vai criar na sequencia até o infinito de IDs no BD, começando em 1
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // Long não precisa ser posto no contrutor, o java la usa ele no BD
-
     private String nome;
-
     private String email;
-
     private int idade;
 
     @ManyToOne // @ManyToOne um ninja tem uma unica missão
     @JoinColumn(name = "missoes_id") // Foreing key ou chave estrangeira
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
